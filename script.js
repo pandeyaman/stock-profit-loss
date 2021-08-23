184aca46efa910a56131b44485b9350a63ba992a
Aman Pandey <pandey.aman111@gmail.com>
2021-08-24 00:49:26 +0530

    added stock price list

--- script.js
+++ script.js
@@ -3,44 +3,70 @@ const stockName = document.getElementById("stock-name");
 const stockDate = document.getElementById("stock-date");
 const stockQuantity = document.getElementById("stock-quantity");
 const lastTradedPrice = document.getElementById('current-price');
+const remarkAbsoluteValue = document.querySelector(".span-remark-1");
+const remarkPercentageValue = document.querySelector(".span-remark-2");
 
-
-
-console.log(URL)
-
+const companySymbols = {
+     "TCS" : "TCS",
+     "Kotak Mahindra Bank" : "KOTAKBANK",
+     "Tata Consumer Products Ltd" : "TATACONSUM",
+     "ITC" : "ITC",
+     "Nestle India Limited" : "NESTLEIND"
+}
 
 const clickHandler = () => {
-    const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockName.value}.BSE&outputsize=full&apikey=S6GRGMO2EDYWVE0J`;
+    let stock = checkSymbolExists();
+    alert(stock);
+    const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock}.BSE&outputsize=full&apikey=S6GRGMO2EDYWVE0J`;
     console.log(URL)
     let date = stockDate.value;
     fetch(URL).then(response => response.json()).then(data => {
+        console.log(data["Time Series (Daily)"])
         let timeSeries = data["Time Series (Daily)"][date]["4. close"];
         console.log(timeSeries  );
         totalCostPrice = timeSeries * stockQuantity.value;
         console.log(totalCostPrice);
-        calculateProfitLoss(totalCostPrice);
+        calculateProfitLoss(totalCostPrice,timeSeries);
     })
 } 
 
 
-const calculateProfitLoss = (totalPrice) => {
+function checkSymbolExists(){
+    let stock = "";
+    if(stockName.value in companySymbols){
+        stock = companySymbols[stockName.value];
+    }
+    else{
+        stock = stockName.value;
+    }
+    return stock;
+}
+
+
+const calculateProfitLoss = (totalPrice,costPrice) => {
     let currentValue = lastTradedPrice.value * stockQuantity.value;
     console.log(currentValue +" current value");
     console.log(totalPrice +"  Total valuation")
     if(currentValue > totalPrice){
-        profitHandler(currentValue - totalPrice);
+        profitHandler(currentValue - totalPrice,costPrice);
     }
     else{
-        lossHandler(totalPrice - currentValue);
+        lossHandler(totalPrice - currentValue,costPrice);
     }
 }
 
-function profitHandler(profit){
-    alert("Arre profit ho gaya");
+function profitHandler(profit,costPrice){
+    profit = Math.trunc(profit*100)/100;
+    profitPercent = Math.trunc(((profit/costPrice)*100)*100)/100;
+    remarkAbsoluteValue.textContent = `Yay ! You made a profit of ${profit} ₹`;
+    remarkPercentageValue.textContent = `You booked a profit of ${profitPercent}%. Are you Harshad Mehta of 2021 ? 🤨`
 }
 
-function lossHandler(){
-    alert("Doob gaya paisa");
+function lossHandler(loss,costPrice){
+    loss = Math.trunc(Math.abs(loss)*100)/100;
+    lossPercent = Math.trunc(((loss/costPrice)*100)*100)/100;
+    remarkAbsoluteValue.textContent = `Oh oo ! You suffered a loss of ${loss} ₹`;
+    remarkPercentageValue.textContent = `You got a loss of ${lossPercent}%. 🤕`
 }
 
 
